ace5e937cd653bfa0e2e7de3d8710b09e0f890a4
StillANixRookie <gvales2831997@gmail.com>
2020-06-05 20:21:47 +0000

    Fri 05 Jun 2020 20:21:47 GMT

--- config.mk
+++ config.mk
@@ -1,33 +1,31 @@
 # dmenu version
-VERSION = 4.5-tip
+VERSION = 4.9
 
 # paths
 PREFIX = /usr/local
-MANPREFIX = ${PREFIX}/share/man
+MANPREFIX = $(PREFIX)/share/man
 
 X11INC = /usr/X11R6/include
 X11LIB = /usr/X11R6/lib
-
-# Pango
-XFTINC = `pkg-config --cflags xft pango pangoxft`
-XFTLIBS  = -lXrender -lfreetype -lz `pkg-config --libs xft pango pangoxft`
-
-# Imlib
-IMLIBINC = `pkg-config --cflags imlib2 openssl`
-IMLIBLIBS = `pkg-config --libs imlib2 openssl`
 
 # Xinerama, comment if you don't want it
 XINERAMALIBS  = -lXinerama
 XINERAMAFLAGS = -DXINERAMA
 
+# freetype
+FREETYPELIBS = -lfontconfig -lXft
+FREETYPEINC = /usr/include/freetype2
+# OpenBSD (uncomment)
+#FREETYPEINC = $(X11INC)/freetype2
+
 # includes and libs
-INCS = -I${X11INC} ${XFTINC} ${IMLIBINC}
-LIBS = -L${X11LIB} -lX11 ${XINERAMALIBS} ${XFTLIBS} ${IMLIBLIBS}
+INCS = -I$(X11INC) -I$(FREETYPEINC)
+LIBS = -L$(X11LIB) -lX11 $(XINERAMALIBS) $(FREETYPELIBS)
 
 # flags
-CPPFLAGS = -DFORTIFY_SOURCE -D_BSD_SOURCE -D_POSIX_C_SOURCE=200809L -DVERSION=\"${VERSION}\" ${XINERAMAFLAGS}
-CFLAGS   = -ansi -pedantic -Wall -Os ${INCS} ${CPPFLAGS}
-LDFLAGS  = -s ${LIBS}
+CPPFLAGS = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_XOPEN_SOURCE=700 -D_POSIX_C_SOURCE=200809L -DVERSION=\"$(VERSION)\" $(XINERAMAFLAGS)
+CFLAGS   = -std=c99 -pedantic -Wall -Os $(INCS) $(CPPFLAGS)
+LDFLAGS  = $(LIBS)
 
 # compiler and linker
 CC = cc
